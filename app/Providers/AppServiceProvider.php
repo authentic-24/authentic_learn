f575258ab33beb17120c55410caff2063d782ef7
unknown <stack.dev@authentic.com.co>
2025-10-04 01:33:42 -0500

    fix:agregando forzar de https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Carbon::setLocale('es');
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
